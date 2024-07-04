0946af02426bed0a470720f6ec99294d109db895
matheusleite01 <de.leite2014@gmail.com>
2024-07-04 12:34:26 -0300

    feat: add format currency function

---
+++ src/utils/formatCurrency.ts
@@ -0,0 +1,7 @@
+export default function formatCurrency(price: number) {
+  let USDollar = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+  return USDollar.format(price);
+}
