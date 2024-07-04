d569e95e9d317a14518b1595a6596b651f156732
matheusleite01 <de.leite2014@gmail.com>
2024-07-04 16:16:36 -0300

    feat: add handleclick for button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,14 @@
 type ButtonProps = {
   children: React.ReactNode;
+  handleClick?: () => void;
 };
 
-const Button = ({ children }: ButtonProps) => {
+const Button = ({ children, handleClick }: ButtonProps) => {
   return (
-    <button className="flex items-center gap-0.5 bg-purple text-white  text-xs p-2 rounded-md font-bold transition-all duration-150 ease-in hover:bg-black">
+    <button
+      className="flex items-center gap-0.5 bg-purple text-white  text-xs p-2 rounded-md font-bold transition-all duration-150 ease-in hover:bg-black"
+      onClick={handleClick}
+    >
       {children}
     </button>
   );
