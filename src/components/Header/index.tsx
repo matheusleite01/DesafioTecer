8ed96a9800226f0e0c15015ef5ccf3ea61c92314
matheusleite01 <de.leite2014@gmail.com>
2024-07-04 17:22:54 -0300

    feat: add shopping cart count

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,9 @@
+import useGlobalContext from "@/hooks/useGlobalContext";
 import Link from "next/link";
 import { PiShoppingCartLight } from "react-icons/pi";
 
 const Header = () => {
+  const {cartDataProducts} = useGlobalContext();
   return (
     <header className="bg-gray border-b border-b-borderGray">
       <div className="container mx-auto flex justify-between items-center p-2.5">
@@ -14,8 +16,9 @@ const Header = () => {
           </span>
           Dev Store
         </Link>
-        <Link href={"/cart"}>
+        <Link href={"/cart"} className="relative">
           <PiShoppingCartLight size={33} className="cursor-pointer" />
+          <div className="absolute -top-2 -right-2 bg-black text-white w-5 h-[20px] text-center text-xs rounded-full">{cartDataProducts.length}</div>
         </Link>
       </div>
     </header>
