9223be55e83215675dc768c556478ef4a9974596
matheusleite01 <de.leite2014@gmail.com>
2024-07-04 16:18:42 -0300

    feat: add product handling functions
    - Create insertCartProducts and removeCartProducts

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -1,10 +1,27 @@
-import { GlobalContextProps } from "@/types";
-import { createContext } from "react";
+import { GlobalContextProps, ProductProps } from "@/types";
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext({} as GlobalContextProps);
 
 const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
-  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+  const [cartDataProducts, setCartDataProducts] = useState<ProductProps[]>([]);
+
+  const insertCartProducts = (products: ProductProps) => {
+    setCartDataProducts((prev) => [...prev, products]);
+  };
+
+  const removeCartProducts = (id: string) => {
+    const newData = cartDataProducts?.filter(item => item.id !== id);
+    setCartDataProducts(newData);
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{ cartDataProducts, setCartDataProducts, insertCartProducts, removeCartProducts }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 };
 
 export default GlobalProvider;
