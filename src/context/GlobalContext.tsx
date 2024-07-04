2509f929c26c10f921bf01e584d6fb7e584e8824
matheusleite01 <de.leite2014@gmail.com>
2024-07-04 18:13:47 -0300

    feat: added cart saving in localDtorage

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -1,5 +1,5 @@
 import { GlobalContextProps, ProductProps } from "@/types";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const GlobalContext = createContext({} as GlobalContextProps);
 
@@ -7,17 +7,32 @@ const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
   const [cartDataProducts, setCartDataProducts] = useState<ProductProps[]>([]);
 
   const insertCartProducts = (products: ProductProps) => {
-    setCartDataProducts((prev) => [...prev, products]);
+    setCartDataProducts((prev) => {
+      const newProducts = [...prev, products];
+      localStorage.setItem("cartDataProducts", JSON.stringify(newProducts));
+      return newProducts;
+    });
   };
 
   const removeCartProducts = (id: string) => {
-    const newData = cartDataProducts?.filter(item => item.id !== id);
+    const newData = cartDataProducts?.filter((item) => item.id !== id);
     setCartDataProducts(newData);
-  }
+    localStorage.setItem("cartDataProducts", JSON.stringify(newData));
+  };
+
+  useEffect(() => {
+    const localData = localStorage.getItem("cartDataProducts");
+    if (localData) setCartDataProducts(JSON.parse(localData));
+  }, []);
 
   return (
     <GlobalContext.Provider
-      value={{ cartDataProducts, setCartDataProducts, insertCartProducts, removeCartProducts }}
+      value={{
+        cartDataProducts,
+        setCartDataProducts,
+        insertCartProducts,
+        removeCartProducts,
+      }}
     >
       {children}
     </GlobalContext.Provider>
