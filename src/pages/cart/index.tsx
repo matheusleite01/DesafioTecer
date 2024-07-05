252ab60e5ecd71dd6862105bf975ddf8d7876178
matheusleite01 <de.leite2014@gmail.com>
2024-07-05 13:54:15 -0300

    feat: add head for card

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,19 +1,26 @@
 import CartProductList from "@/components/CartProductList";
 import EmptyCart from "@/components/EmptyCart";
 import useGlobalContext from "@/hooks/useGlobalContext";
+import Head from "next/head";
 import React from "react";
 
 const CartPage = () => {
   const { cartDataProducts } = useGlobalContext();
 
   return (
-    <div className="container mx-auto">
-      {cartDataProducts.length ? (
-        <CartProductList cartProducts={cartDataProducts} />
-      ) : (
-        <EmptyCart />
-      )}
-    </div>
+    <>
+    <Head>
+        <title>Cart</title>
+        <meta name="description" content="Dev Store products" />
+    </Head>
+      <div className="container mx-auto">
+        {cartDataProducts.length ? (
+          <CartProductList cartProducts={cartDataProducts} />
+        ) : (
+          <EmptyCart />
+        )}
+      </div>
+    </>
   );
 };
 
