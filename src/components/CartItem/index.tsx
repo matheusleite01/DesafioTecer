a0d1d101f2d8c032c441a5370b9815549582f4f9
matheusleite01 <de.leite2014@gmail.com>
2024-07-07 16:48:24 -0300

    feat: add CartItem component

---
+++ src/components/CartItem/index.tsx
@@ -0,0 +1,47 @@
+import useGlobalContext from "@/hooks/useGlobalContext";
+import { ProductProps } from "@/types";
+import formatCurrency from "@/utils/formatCurrency";
+import quantityChange from "@/utils/quantityChange";
+import Image from "next/image";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { FaMinusCircle } from "react-icons/fa";
+
+type CardItemProps = {
+  cardProduct: ProductProps;
+};
+
+const CardItem = ({ cardProduct }: CardItemProps) => {
+  const { increaseQuantity } = useGlobalContext();
+  return (
+    <div className="flex flex-col items-center gap-1">
+      <Image
+        src={cardProduct?.image}
+        width={50}
+        height={50}
+        alt={cardProduct?.title}
+      />
+      <span className="text-xs font-bold">
+        {formatCurrency(cardProduct?.price)}
+      </span>
+      <div className="flex items-center gap-3">
+        <FaMinusCircle
+          size={16}
+          color="#DFDFDF"
+          className="cursor-pointer"
+          onClick={() => quantityChange("minus", cardProduct, increaseQuantity)}
+        />
+        <span className="font-bold text-xs">{cardProduct?.quantity}</span>
+        <AiFillPlusCircle
+          size={19}
+          color="#DFDFDF"
+          className="cursor-pointer"
+          onClick={() =>
+            quantityChange("increase", cardProduct, increaseQuantity)
+          }
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CardItem;
